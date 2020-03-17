770a5a5ac52634c6f9e3555779bd3db3516e046a
Julien <julien.planque-ext@galec.fr>
2020-03-17 12:31:34 +0100

    feat: add switch, 404 and history

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Accueil from './Accueil'
 import Contact from './Contact'
 import Projets from './Projets'
 import Navigation from './Navigation'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Navigation />
-        <Route path="/" exact component={Accueil} />
-        <Route path="/Projets" exact component={Projets} />
-        <Route path="/Contact" exact component={Contact} />
-
+      <Router
+        forceRefresh={true}>
+        <Navigation />
+        <Switch> {/* Permet de gérer les erreurs 404 */}
+          <Route path="/" exact component={Accueil} />
+          <Route path="/Projets" exact component={Projets} />
+          <Route path="/Contact" exact component={Contact} />
+          <Route path="/" component={() => <div>ERREUR 404</div>} />
+        </Switch>
       </Router>
     </div>
   );
--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default function Contact() {
+    let history = useHistory();
+
     return (
-        <h1>SECTION CONTACT</h1>
+        <div>
+            <h1>SECTION CONTACT</h1>
+            <button onClick={() => history.push('/')}>Go To racine</button>
+        </div>
     )
 }
